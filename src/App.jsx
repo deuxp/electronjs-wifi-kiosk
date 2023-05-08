da2fe437cd34d079583d6aa000482e0da338f5fb
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-05-07 22:51:21 -0400

    creating the form and handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
+import { useEffect, useState } from "react";
 import style from "./App.module.css";
 import Button from "./components/Button/Button";
 
 function App() {
-  function handleClick() {
-    console.log("click");
+  const [password, setPassword] = useState("");
+  const [networks, setNetworks] = useState([]);
+  const [netSelect, setNetSelect] = useState("");
+
+  async function getNetworks() {
+    const res = await window.api.getData("networks");
+    console.log(res);
+    setNetworks(res);
   }
+
+  const networkNames = netArr => {
+    const networks = netArr.map((net, idx) => {
+      return (
+        <option key={idx + 1} value={net.ssid}>
+          {net.ssid}
+        </option>
+      );
+    });
+    const defaultOption = <option key={0}> -- select a network -- </option>;
+    return [defaultOption, ...networks];
+  };
+
+  const handleChange = e => {
+    const select = e.target.value;
+    setNetSelect(select);
+  };
+
+  const handleClick = () => {
+    console.log({ network: netSelect, password });
+  };
+
+  useEffect(() => {
+    getNetworks();
+  }, []);
+
   return (
     <div className={style.main}>
       <div className={style.container}>
-        <div>hello</div>
+        <div className={style.title}>Wifi Networks</div>
+        <select
+          defaultValue={netSelect}
+          // value={netSelect}
+          onChange={handleChange}
+        >
+          {networkNames(networks)}
+        </select>
+        <input
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          placeholder="password"
+        />
         <Button handleClick={handleClick}>Click</Button>
       </div>
     </div>
