5a128eda12f73eb92e772185eeadc53f8d2d35b3
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-05-09 16:24:31 -0400

    del note

--- src/services/wifi.js
+++ src/services/wifi.js
@@ -10,21 +10,3 @@ async function connectWifi(ssid, password) {
 }
 
 module.exports = { getNetworks, connectWifi };
-
-/* ---------- DATA SHAPE:
-networks = [
-    {
-      ssid: '...',
-      bssid: '...',
-      mac: '...', // equals to bssid (for retrocompatibility)
-      channel: <number>,
-      frequency: <number>, // in MHz
-      signal_level: <number>, // in dB
-      quality: <number>, // same as signal level but in %
-      security: 'WPA WPA2' // format depending on locale for open networks in Windows
-      security_flags: '...' // encryption protocols (format currently depending of the OS)
-      mode: '...' // network mode like Infra (format currently depending of the OS)
-    },
-    ...
-];
-*/
