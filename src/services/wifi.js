545344b3d2086dd46c061179ad9e51bf063c95db
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-05-10 21:59:46 -0400

    debounce getNetworks()

--- src/services/wifi.js
+++ src/services/wifi.js
@@ -1,26 +1,47 @@
 const wifi = require("node-wifi");
 
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true);
+    }, ms);
+  });
+}
+
 async function getNetworks() {
   wifi.init({ iface: null });
-  return new Promise((resolve, reject) => {
-    wifi
-      .scan()
-      .then(res => {
-        return res;
-      })
-      .then(res => {
-        if (!res?.length) {
-          reject({ data: null, message: "error: refresh wifi" });
-        }
-        return res;
-      })
-      .then(res => {
-        resolve({ data: res, message: "wifi init: success" });
-      })
-      .catch(error => {
-        reject({ data: null, message: "problem: check wifi" });
-      });
-  });
+  await sleep(1000);
+  try {
+    let res = await wifi.scan();
+
+    if (res?.length === 0) {
+      await sleep(1000);
+      console.log("loop");
+      res = await wifi.scan();
+    }
+    return { data: res, message: "node-wifi.scan" };
+  } catch (error) {
+    return { data: null, message: "error: refresh wifi" };
+  }
+  // return new Promise((resolve, reject) => {
+  //   wifi
+  //     .scan()
+  //     .then(res => {
+  //       return res;
+  //     })
+  //     .then(res => {
+  //       if (!res?.length) {
+  //         reject({ data: null, message: "error: refresh wifi" });
+  //       }
+  //       return res;
+  //     })
+  //     .then(res => {
+  //       resolve({ data: res, message: "wifi init: success" });
+  //     })
+  //     .catch(error => {
+  //       reject({ data: null, message: "problem: check wifi" });
+  //     });
+  // });
 }
 
 async function connectWifi(ssid, password) {
